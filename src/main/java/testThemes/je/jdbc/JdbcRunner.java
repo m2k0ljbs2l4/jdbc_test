9f076fa86665f4ad267d50489e3717dd6055ef0e
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-07 01:11:13 +0500

    add method getTicketsByFlightId

--- src/main/java/testThemes/je/jdbc/JdbcRunner.java
+++ src/main/java/testThemes/je/jdbc/JdbcRunner.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import testThemes.je.jdbc.utils.ConnectionManager;
 
@@ -12,7 +14,7 @@ public class JdbcRunner {
 	public static void main(String[] args) throws SQLException {
 		
 		String sql = "CREATE SCHEMA game; ";
-		String sqlQuery = "SELECT * FROM ticket";
+		String sqlQuery = "SELECT * FROM ticket;";
 //		String sql = "DROP SCHEMA game; ";
 
 		try (Connection connection = ConnectionManager.open(); 
@@ -26,6 +28,21 @@ public class JdbcRunner {
 				System.out.println(result.getString("passanger_name"));
 				System.out.println(result.getObject("id"));
 		}
+	}
+	
+	public static List<Long> getTicketsByFlightId(Long flightId) {
+		List<Long> tickets = new ArrayList<Long>();
+		String sql = "SELECT * FROM ticket WHERE flight_id = %s".formatted(flightId);
+		try (Connection connection = ConnectionManager.open();
+				Statement statement = connection.createStatement()) {
+			ResultSet result = statement.executeQuery(sql);
+			while (result.next()) {
+				tickets.add(result.getLong("id"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return tickets;
 	}
 
 }
