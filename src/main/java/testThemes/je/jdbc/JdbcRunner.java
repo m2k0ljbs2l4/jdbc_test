6e335a92c8df4437ce18abb2c6882bedabf69e58
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-07 00:54:18 +0500

    update JdbcRunner add sql query

--- src/main/java/testThemes/je/jdbc/JdbcRunner.java
+++ src/main/java/testThemes/je/jdbc/JdbcRunner.java
@@ -2,15 +2,20 @@ package testThemes.je.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import testThemes.je.jdbc.utils.ConnectionManager;
 
 public class JdbcRunner {
 
 	public static void main(String[] args) throws SQLException {
+		
+		String sql = "create schema game; ";
+//		String sql = "drop schema game; ";
 
-		try (Connection connection = ConnectionManager.open()) {
-			System.out.println(connection.getTransactionIsolation());;
+		try (Connection connection = ConnectionManager.open(); 
+				Statement statement = connection.createStatement()) {
+			System.out.println(statement.execute(sql));;
 		}
 	}
 
