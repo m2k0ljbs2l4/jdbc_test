1f6c8599e5f5438768e02f7ec909b12a2b6cca9a
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-07 01:03:53 +0500

    add executeQuery (select)

--- src/main/java/testThemes/je/jdbc/JdbcRunner.java
+++ src/main/java/testThemes/je/jdbc/JdbcRunner.java
@@ -1,6 +1,7 @@
 package testThemes.je.jdbc;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,12 +11,20 @@ public class JdbcRunner {
 
 	public static void main(String[] args) throws SQLException {
 		
-		String sql = "create schema game; ";
-//		String sql = "drop schema game; ";
+		String sql = "CREATE SCHEMA game; ";
+		String sqlQuery = "SELECT * FROM ticket";
+//		String sql = "DROP SCHEMA game; ";
 
 		try (Connection connection = ConnectionManager.open(); 
 				Statement statement = connection.createStatement()) {
-			System.out.println(statement.execute(sql));;
+			System.out.println(statement.execute(sql));
+			ResultSet result = statement.executeQuery(sqlQuery);
+			while(result.next())
+				System.out.println(result.getLong("id"));
+				System.out.println(result.getLong("flight_id"));
+				System.out.println(result.getLong("cost"));
+				System.out.println(result.getString("passanger_name"));
+				System.out.println(result.getObject("id"));
 		}
 	}
 
