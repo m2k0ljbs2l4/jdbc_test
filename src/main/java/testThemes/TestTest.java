846c4d430624317bb72da282dd5861063db64627
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-06 19:09:45 +0500

    Add JDBC Runner and update pom dependencies

--- src/main/java/testThemes/TestTest.java
+++ src/main/java/testThemes/TestTest.java
@@ -4,7 +4,7 @@ public class TestTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("FirstName");
 	}
 
 }
